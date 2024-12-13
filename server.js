3583c7bae15eaf344947680f590343e4b669387d
= <racem.hachicha1@gmail.com>
2024-12-13 20:25:37 +0100

    Backend deployment

--- server.js
+++ server.js
@@ -2,6 +2,4 @@ require("dotenv").config();
 const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
