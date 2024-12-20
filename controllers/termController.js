b145d2df05a852deea4c36b1e96b1cf1836a3d3d
= <racem.hachicha1@gmail.com>
2024-12-20 02:40:43 +0100

    Added terms functionality

--- controllers/termController.js
+++ controllers/termController.js
@@ -2,18 +2,23 @@ const { fetchGraphQL } = require("../models/graphqlModel");
 
 const getTerms = async (req, res) => {
   const query = `{constants(type_id:"term"){id name}}`;
-  const reponse = await fetchGraphQL(query);
 
-  if (reponse.ok) {
-    const data = await reponse.json();
-    const terms = data.data.constants;
-    const currentTerm = getCurrentTerm(terms);
-    res.send({
-      terms: terms,
-      currentTerm: currentTerm,
-    });
-  } else {
-    res.status(reponse.status).send(reponse.statusText);
+  try {
+    const reponse = await fetchGraphQL(query);
+    if (reponse.ok) {
+      const data = await reponse.json();
+      const terms = data.data.constants;
+      const currentTerm = getCurrentTerm(terms);
+      res.send({
+        terms: terms,
+        currentTerm: currentTerm,
+      });
+    } else {
+      res.status(reponse.status).send(reponse.statusText);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
 };
 
