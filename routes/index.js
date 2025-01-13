9cc7d21724fe0cb5c3c7a54f501bcc2034fc72f0
= <racem.hachicha1@gmail.com>
2025-01-13 17:01:59 +0100

    Finalizing the project

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,4 @@ apiRouter.use("/regions", regionsRoutes);
 apiRouter.use("/terms", termsRoutes);
 apiRouter.use("/user", userRoutes);
 
-export default { apiRouter, authRouter };
+export default { apiRouter };
